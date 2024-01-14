8d1b4314978a8b6c8557832d2b0418184281dcc1
AnshulGoyal589 <anshulgoyal589@gmail.com>
2024-01-14 14:46:36 +0530

    version1

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ let toggle=false;
 let name;
 
 // mongoose.connect('mongodb://127.0.0.1:27017/kiosk')
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> console.log("db connected sucessfully".yellow)) 
 .catch((err)=> console.log(err));
 
